3c897e59b43b2bb562a56891e6aa8019bc3c1bdf
gitYord <hai-kbrobeil@web.de>
2014-09-02 20:32:45 +0200

    add option to disable auto start after boot if receiver was enabled before that

--- src/de/adornis/Notifier/StartupReceiver.java
+++ src/de/adornis/Notifier/StartupReceiver.java
@@ -3,12 +3,14 @@ package de.adornis.Notifier;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class StartupReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if(PreferenceManager.getDefaultSharedPreferences(context).getBoolean("receiver_online", false)) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		if(prefs.getBoolean("receiver_online", false) && prefs.getBoolean("start_after_boot", true)) {
 			context.startService(new Intent(context, Listener.class));
 		}
 	}
